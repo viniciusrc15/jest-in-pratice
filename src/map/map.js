084b5c891095c2b5c5c7e58d98abbb95a4c7e562
Vinicius Castro <vinicius1595@gmail.com>
2019-05-17 18:18:21 -0300

    Implement more tests in function map

--- src/map/map.js
+++ src/map/map.js
@@ -1,9 +1,15 @@
 'use strict'
 
-export default function map(arr, func) {
+export default function map(arr = [], func = (item) => item) {
+    /*if(!Array.isArray(arr))
+        throw new TypeError('Array must be passed')
+
+    if (!func || typeof func !== 'function')
+        throw new TypeError('Function must be passed')*/
+
     let newarray = []
     for (let i = 0; i < arr.length; i++) {
-        newarray.push(func(arr[i]))
+        newarray.push(func(arr[i], i, arr))
     }
     return newarray
 }
--- src/map/map.test.js
+++ src/map/map.test.js
@@ -15,7 +15,24 @@ it('map([3,4], (item) => item) should return [3,4]', () => {
     expect(map([3, 4], (item) => item)).to.be.deep.equal([3, 4])
 })
 
-it('map([3,4], (item) => item + 1) should return [3,4]', () => {
-    expect(map([3, 4], (item) => item + 1)).to.be.deep.equal([4, 5])
+it('map([3,4], (item, index) => index) should return [0,1]', () => {
+    expect(map([3, 4], (item, index) => index)).to.be.deep.equal([0, 1])
 })
+
+it('map([3,4], (item, index, array) => array) should return [[3,4],[3,4]]', () => {
+    expect(map([3, 4], (item, index, array) => array)).to.be.deep.equal([[3,4],[3,4]])
+})
+
+/*it('map([3,4]) should throw an error with message "Function must be passed"', () => {
+    expect(map[3, 4]).to.throw("Function must be passed")
+})*/
+
+it('map([3,4]) should return [3,4]', () => {
+    expect(map([3, 4])).to.be.deep.equal([3, 4])
+})
+
+it('map() shoul return []', () => {
+    expect(map()).to.be.deep.equal([])
+})
+
 
